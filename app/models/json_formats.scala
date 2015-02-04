8c05778b6c5e8285486388a5e80c7c38681ef2ae
Alexey Tkachenko <iskaspb@gmail.com>
2015-02-05 06:47:49 +0800

    fix compile error

--- app/models/json_formats.scala
+++ app/models/json_formats.scala
@@ -3,7 +3,5 @@ package models
 object JsonFormats {
   import play.api.libs.json.Json
 
-  // Generates Writes and Reads for Product and User thanks to Json Macros
-  implicit val userFormat = Json.format[User]
   implicit val productFormat = Json.format[Product]
 }
